2dcf1279b886841788f0c5428703180b149ad2e4
xchen <xchen601@gmail.com>
2023-06-04 00:55:31 -0400

    finished navbar & hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { styles } from '../styles'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+        <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+            <div>
+                <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className='text-red-500'>Xing</span></h1>
+                <p className={`${styles.heroSubText} text-white`}>I'm a college student, web developer, <br className="sm:block hidden" /> and learning about ML & AI</p>
+            </div>
+
+        </div>
+        
+    </section>
   )
 }
 
